73c49e57ec3703e51c8e68213aeff6082c462a81
sethreno <sethreno@gmail.com>
2012-09-12 00:52:47 -0500

    fixes #1 Got order status working.

--- main.py
+++ main.py
@@ -9,14 +9,18 @@ jinja_env = jinja2.Environment(
 from google.appengine.api import users
 from google.appengine.ext import db
 
+ingredients = (
+	'Rice', 'Turkey', 'Peppers', 'Jalepenos', 'Potatoes', 'Corn', 'Cheese')
+statuses = (
+	'queued', 'preparing', 'cooking', 'ready', 'served')
+
 class Order(db.Model):
 	name =db.StringProperty()
 	ingredients = db.StringListProperty()
+	status = db.StringProperty()
 
 class OrderForm(webapp2.RequestHandler):
 	def get(self):
-		ingredients = (
-			'Rice', 'Turkey', 'Peppers', 'Jalepenos', 'Potatoes', 'Corn', 'Cheese')
 		vals = {'ingredients': ingredients}
 		t = jinja_env.get_template('OrderForm.html')
 		self.response.out.write(t.render(vals))
@@ -26,6 +30,7 @@ class OrderSave(webapp2.RequestHandler):
 		order = Order()
 		order.name = self.request.get('name')
 		order.ingredients = self.request.get_all('ingredients')
+		order.status = "queued"
 		order.put()
 		self.redirect('/OrderThanks')
 
@@ -35,16 +40,26 @@ class OrderThanks(webapp2.RequestHandler):
 
 class ViewOpenOrders(webapp2.RequestHandler):
 	def get(self):
-		orders = db.GqlQuery("SELECT * FROM Order")
-		for o in orders:
-			self.response.out.write('<div>')
-			self.response.out.write(cgi.escape(o.name) + ' : ' + ' '.join(o.ingredients))
-			self.response.out.write('</div>')
+		orders = db.GqlQuery("SELECT * FROM Order WHERE status != 'served'")
+		vals = {'orders': orders, 'statuses': statuses}
+		t = jinja_env.get_template('ActiveOrderList.html')
+		self.response.out.write(t.render(vals))
+
+	def post(self):
+		keys = self.request.get_all('key')
+		status = self.request.get_all('status')
+		for i in range(len(keys)):
+			o = db.get(keys[i])
+			o.status = status[i]
+			o.put()
+
+		self.redirect('/ViewOpenOrders')
 
 app = webapp2.WSGIApplication([
 		('/', OrderForm)
 		,('/OrderSave', OrderSave)
 		,('/OrderThanks', OrderThanks)
 		,('/ViewOpenOrders', ViewOpenOrders)
+		,('/ViewOpenOrders/Update', ViewOpenOrders)
 	], debug=True)
 
